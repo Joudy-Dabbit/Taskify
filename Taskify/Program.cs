b369771b90a973d5319531a7203a910b8b8e2033
Joudy Dabbit <joudy.6.dabbit@gmail.com>
2022-11-24 12:31:51 +0300

    .

--- Taskify/Program.cs
+++ Taskify/Program.cs
@@ -33,11 +33,10 @@ builder.Services.AddScoped<ITodoRepository, TodoRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
